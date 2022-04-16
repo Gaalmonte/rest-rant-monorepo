039fefa0c1faf6ce6c28bc3a1fd7bf6934db7b04
German <german.a.almonte@gmail.com>
2022-04-16 14:39:06 -0400

    finished jwt auth activity

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
-// Modules and Globals
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-
-// Express Settings
-app.use(cors());
+const defineCurrentUser = require("./middleware/defineCurrentUser");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(defineCurrentUser);
 
 // Controllers & Routes
 
